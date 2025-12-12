29af0e3b915d7eff6a8d41fa9fd775dba6704bd6
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-12 13:10:23 +0700

    feat: Implement Ui

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What Do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You Have X item on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
